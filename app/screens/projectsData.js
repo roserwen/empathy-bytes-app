c18af15b947fcdc610a2cf1796c86ef83881c4ac
genesis <116662964+genefu@users.noreply.github.com>
2023-10-19 13:50:00 -0400

    Adds pushing to nav stack functionality, doesn't work yet, but the button still shows up in the list so that's good

---
+++ app/screens/projectsData.js
@@ -0,0 +1,5 @@
+export const projectData = [
+    {id: 1, name: "project 1", description: "amazing project1"//, imgSrc: "imgepath"
+    },
+    {id: 2, name: "project 2", description: "amzing project2"}
+];
