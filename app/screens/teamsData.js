26fd94815f7000de25da5b86080a7239c83387f8
genesis <116662964+genefu@users.noreply.github.com>
2023-10-21 12:22:19 -0400

    Adds the json page rendering to the team pages

---
+++ app/screens/teamsData.js
@@ -0,0 +1,6 @@
+export const teamData = [
+    {id: 1, name: "App Team", description: "this is the app team"},
+    {id: 2, name: "Web Team", description: "this is the web team"},
+    {id: 3, name: "VR/AR Team", description: "this is the VR/AR team"},
+    {id: 4, name: "Media Team", description: "this is the media team"}
+]
